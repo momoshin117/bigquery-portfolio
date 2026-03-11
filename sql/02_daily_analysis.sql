aaaf1fc672e9ecb2307201d789704e225add2618
momoshin117 <shintaro.cm@gmail.com>
2026-03-11 20:46:17 +0900

    02日別集計コメント修正

--- sql/02_daily_analysis.sql
+++ sql/02_daily_analysis.sql
@@ -9,11 +9,13 @@ CREATE OR REPLACE TABLE `bigquery-portfolio-488907.portfolio.kpi_daily` AS
 SELECT
   DATE(o.created_at) AS order_date,
   SUM(i.sale_price) AS sales,
+
   -- INNER JOIN のため、1注文に複数商品を注文すると、order_id, user_id ともに
   -- 同じ id が複数行にわたるため、重複を考慮したカウントが必要
   COUNT(DISTINCT o.order_id) AS order_count,
   COUNT(DISTINCT o.user_id) AS purchasing_users,
   SAFE_DIVIDE(SUM(i.sale_price), COUNT(DISTINCT o.order_id)) AS average_purchase_price
+  
 -- 売上は order_items にしかないため、INNER JOIN
 -- order_items にないものは除外
 FROM `bigquery-public-data.thelook_ecommerce.orders` o
